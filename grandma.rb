3fa17c675c05a50b49d7e3854e1062e5304eb6cc
Kimberly Baker <bakerk2010@gmail.com>
2020-07-19 20:34:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,19 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
-
-speak_to_grandma("I LOVE YOU GRANDMA!")
-   
-speak_to_grandma("Hi Nana, how are you?")
-
-speak_to_grandma("Hi!")
-
-speak_to_grandma("WHAT DID YOU EAT TODAY?")
-
-speak_to_grandma("WHAT?")
